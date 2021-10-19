323812b66a739ca81cdf9f538868a5a54ba83320
geekaholick <33748869+geekaholick@users.noreply.github.com>
2021-10-19 19:04:39 +0800

    T-01 Navigation
    - added logo
    - in page navigation
    - link to usc mpc portal

--- src/uscmpc/NavBar.vue
+++ src/uscmpc/NavBar.vue
@@ -1,12 +1,15 @@
 <template>
-    <header class="header-main header-main2" :class="{'sticky': scrolled}">
+    <header class="header-main header-main2" :class="{ sticky: scrolled }">
         <div class="container">
             <div class="row">
                 <div class="col-12">
                     <div class="menu-wrap">
                         <div class="brand-logo">
                             <a href="#">
-                                <img src="../assets/img/home-medical/logo/logo-2.png" alt="Brand Logo">
+                                <img
+                                    src="../assets/img/home-uscmpc/logo/logo.png"
+                                    alt="Brand Logo"
+                                />
                             </a>
                         </div>
                         <div id="hamburger" @click="display_menu()">
@@ -15,14 +18,29 @@
                             <span></span>
                         </div>
                         <ul class="main-menu main-menu__style-2">
-                            <li><a @click="close_menu()" href="#home" class="activee">Home</a></li>
-                            <li><a @click="close_menu()" href="#appointment">Appointment</a></li>
-                            <li><a @click="close_menu()" href="#service">Service</a></li>
-                            <li><a @click="close_menu()" href="#review">Testimonial</a></li>
-                            <li><a @click="close_menu()" href="#team">Team</a></li>
-                            <li><a @click="close_menu()" href="#department">Department</a></li>
-                            <li><a @click="close_menu()" href="#blog">Blog</a></li>
-                            <li><a @click="close_menu()" href="#support">Support</a></li>
+                            <li class="test">
+                                <a
+                                    @click="close_menu()"
+                                    href="#home"
+                                    class="activee"
+                                    >Home</a
+                                >
+                            </li>
+                            <li class="test">
+                                <a @click="close_menu()" href="#service"
+                                    >Product and Service</a
+                                >
+                            </li>
+                            <li class="test">
+                                <a @click="close_menu()" href="#about"
+                                    >About Us</a
+                                >
+                            </li>
+                            <li>
+                                <a href="http://www.event.uscmpc.com/login"
+                                    >Member's Portal</a
+                                >
+                            </li>
                         </ul>
                     </div>
                 </div>
@@ -32,108 +50,128 @@
 </template>
 
 <script>
-    export default {
-		name: 'Navbar',
-		data () {
-			return {
-                load: false,
-                limitPosition: 200,
-                scrolled: false,
-                lastPosition: 500
-			}
-		},
-		methods: {
-            // responsive menu script
-            display_menu : function(){
-                var body = document.getElementsByTagName("body")[0];
-                (!body.classList.contains("display_menu")) ? body.classList.add("display_menu") : body.classList.remove("display_menu");
-            },
-            close_menu : function(){
-				var body = document.getElementsByTagName("body")[0];
-				body.classList.remove('display_menu');
-            },
-            loaded : function(){
-                document.getElementsByTagName("body")[0].style.overflowY = "hidden";
-                this.load = true;
-            },
-
-            // sticky menu script
-            handleScroll() {
-                if (this.lastPosition < window.scrollY && this.limitPosition < window.scrollY) {
-                    this.scrolled = true;
-                    // move up!
-                } 
-                
-                if (this.lastPosition > window.scrollY) {
-                    this.scrolled = true;
-                    // move down
-                }
-                
-                this.lastPosition = window.scrollY;
-                this.scrolled = window.scrollY > 50;
-            }
-        }, 
-        created() {
-            window.addEventListener("scroll", this.handleScroll);
+// import Dropdown from '../components/Dropdown'
+export default {
+    name: "Navbar",
+    data() {
+        return {
+            load: false,
+            limitPosition: 200,
+            scrolled: false,
+            lastPosition: 500,
+        };
+    },
+    methods: {
+        // responsive menu script
+        display_menu: function () {
+            var body = document.getElementsByTagName("body")[0];
+            !body.classList.contains("display_menu")
+                ? body.classList.add("display_menu")
+                : body.classList.remove("display_menu");
         },
-        destroyed() {
-            window.removeEventListener("scroll", this.handleScroll);
+        close_menu: function () {
+            var body = document.getElementsByTagName("body")[0];
+            body.classList.remove("display_menu");
+        },
+        loaded: function () {
+            document.getElementsByTagName("body")[0].style.overflowY = "hidden";
+            this.load = true;
         },
 
-        mounted() {
-            (function() {
-                scrollTo();
-            })();
-
-            function scrollTo() {
-                const links = document.querySelectorAll('.main-menu li a');
-                links.forEach(each => (each.onclick = scrollAnchors));
+        // sticky menu script
+        handleScroll() {
+            if (
+                this.lastPosition < window.scrollY &&
+                this.limitPosition < window.scrollY
+            ) {
+                this.scrolled = true;
+                // move up!
             }
 
-            function scrollAnchors(e, respond = null) {
-                const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
-                e.preventDefault();
-                var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
-                const targetAnchor = document.querySelector(targetID);
-                if (!targetAnchor) return;
-                const originalTop = distanceToTop(targetAnchor);
-                window.scrollBy({ top: originalTop - 75, left: 0, behavior: 'smooth' });
-                const checkIfDone = setInterval(function() {
-                    const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 0;
-                    if (distanceToTop(targetAnchor) === 0 || atBottom) {
-                        targetAnchor.tabIndex = '-1';
-                        targetAnchor.focus();
-                        clearInterval(checkIfDone);
-                    }
-                }, 800);
+            if (this.lastPosition > window.scrollY) {
+                this.scrolled = true;
+                // move down
             }
-            //scroll spy js
-            window.addEventListener('load', function(){
-                var section = document.querySelectorAll(".main-container > *");
-                var sections = {};
-                var i = 0;
 
-                Array.prototype.forEach.call(section, function(e) {
-                    sections[e.id] = e.offsetTop - 85;
-                });
+            this.lastPosition = window.scrollY;
+            this.scrolled = window.scrollY > 50;
+        },
+    },
+    created() {
+        window.addEventListener("scroll", this.handleScroll);
+    },
+    destroyed() {
+        window.removeEventListener("scroll", this.handleScroll);
+    },
 
-                window.onscroll = function() {
-                    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+    mounted() {
+        (function () {
+            scrollTo();
+        })();
 
-                    for (i in sections) {
-                        if (sections[i] <= scrollPosition) {
-                            document.querySelector('.activee').setAttribute('class', ' ');
-                            document.querySelector('a[href*=' + i + ']').setAttribute('class', 'activee');
-                        }
-                    }
-                };
-            });
+        function scrollTo() {
+            const links = document.querySelectorAll(".main-menu .test a");
+            links.forEach((each) => (each.onclick = scrollAnchors));
         }
-    }
-    
+
+        function scrollAnchors(e, respond = null) {
+            const distanceToTop = (el) =>
+                Math.floor(el.getBoundingClientRect().top);
+            e.preventDefault();
+            var targetID = respond
+                ? respond.getAttribute("href")
+                : this.getAttribute("href");
+            const targetAnchor = document.querySelector(targetID);
+            if (!targetAnchor) return;
+            const originalTop = distanceToTop(targetAnchor);
+            window.scrollBy({
+                top: originalTop - 75,
+                left: 0,
+                behavior: "smooth",
+            });
+            const checkIfDone = setInterval(function () {
+                const atBottom =
+                    window.innerHeight + window.pageYOffset >=
+                    document.body.offsetHeight - 0;
+                if (distanceToTop(targetAnchor) === 0 || atBottom) {
+                    targetAnchor.tabIndex = "-1";
+                    targetAnchor.focus();
+                    clearInterval(checkIfDone);
+                }
+            }, 800);
+        }
+        //scroll spy js
+        window.addEventListener("load", function () {
+            var section = document.querySelectorAll(".main-container > *");
+            var sections = {};
+            var i = 0;
+
+            Array.prototype.forEach.call(section, function (e) {
+                sections[e.id] = e.offsetTop - 85;
+            });
+
+            window.onscroll = function () {
+                var scrollPosition =
+                    document.documentElement.scrollTop ||
+                    document.body.scrollTop;
+
+                for (i in sections) {
+                    if (sections[i] <= scrollPosition) {
+                        document
+                            .querySelector(".activee")
+                            .setAttribute("class", " ");
+                        document
+                            .querySelector("a[href*=" + i + "]")
+                            .setAttribute("class", "activee");
+                    }
+                }
+            };
+        });
+    },
+};
 </script>
 
 <style lang='scss'>
-	@import '../assets/scss/variables.scss';
-	@import '../assets/scss/components/header.scss';
+@import "../assets/scss/variables.scss";
+@import "../assets/scss/components/header.scss";
 </style>
