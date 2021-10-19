a711c18fc808f35c62ef6cf5121bd537d4c07ae5
Rex <jrpana89@gmail.com>
2021-10-20 00:51:28 +0800

    T-03 About
    - Innitials Pages for About

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,10 @@
 import Home from '../pages/Home'
+import History from '../pages/History'
+import CooperativeProfile from '../pages/CooperativeProfile'
+import BoardOfDirectors from '../pages/BoardOfDirectors'
+import CooperativeOfficers from '../pages/CooperativeOfficers'
+import AwardsDistinctions from '../pages/AwardsDistinctions'
+import PhotosVideos from '../pages/PhotosVideos'
 import NotFound from '../pages/404'
 import Coming_soon from '../pages/Coming_soon'
 
@@ -18,4 +24,36 @@ export const routes = [
         name: 'Home',
         component: Home
     },  
+    {
+        path:'/history',
+        name: 'History',
+        component: History
+    }, 
+     
+    {
+        path:'/cooperative-profile',
+        name: 'Cooperative Profile',
+        component: CooperativeProfile
+    }, 
+    {
+        path:'/board-of-directors',
+        name: 'Board of Directors',
+        component: BoardOfDirectors
+    }, 
+    {
+        path:'/cooperative-officers',
+        name: 'Cooperative Officers',
+        component: CooperativeOfficers
+    },
+    {
+        path:'/awards-distinctions',
+        name: 'Awards and Distictions',
+        component: AwardsDistinctions
+    },
+    {
+        path:'/photos-videos',
+        name: 'Photos and Videos',
+        component: PhotosVideos
+    },
+    
 ];
