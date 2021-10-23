3a4bc6cb94dfa0e6972422f6e1d2bba87ac54d03
Edrian Guanzon <guanzonedrian@gmail.com>
2021-10-23 16:12:14 +0800

    feat(BOD): update board of directors page

--- src/uscmpc/BoardOfDirectors.vue
+++ src/uscmpc/BoardOfDirectors.vue
@@ -1,53 +1,133 @@
 <template>
-    <section class="video-section section-padding fix" :style="bgImg">
+    <section class="testimonial-area pt-100 fix section" id="review" :style="bgImg">
         <div class="container">
             <div class="row">
                 <div class="col-12">
                     <div class="section-title title-style-4 text-center">
                         <h2 class="title">BOARD OF DIRECTORS</h2>
-                        <p class="paragraph">Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum.</p>
                     </div>
                 </div>
             </div>
             <div class="row">
-                <div class="col-lg-12">
-                    <div class="bestapp-content-inner">
-                        <div class="bestapp-content-text pb-0">
-                            <p class="paragraph">Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima.</p>
-                            <ul class="video-list video-list__style-2">
-                                <li><i class="fa fa-check-square-o"></i> Perfect layout</li>
-                                <li><i class="fa fa-check-square-o"></i> Sweet moves</li>
-                            </ul>
+                <div class="col-4">
+                    <div class="custom_testimonial-item">
+                        <div class="custom_testimonial-thumb">
+                            <img src="../assets/img/bod/glarino.jpg" alt="client thumb">
+                        </div>
+                        <div class="testimonial-content">
+                            <h6 class="custom_client">Glenn Glarino</h6>
+                            <p style="color: black">Chair</p>
                         </div>
                     </div>
                 </div>
-                <!-- <div class="col-lg-6">
-                    <div class="video-player">
-                        <figure class="embed-responsive embed-responsive-16by9">
-                            <iframe :src="video" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                        </figure>
+                <div class="col-4">
+                    <div class="custom_testimonial-item">
+                        <div class="custom_testimonial-thumb">
+                            <img src="../assets/img/bod/abrenica.jpg" alt="client thumb">
+                        </div>
+                        <div class="testimonial-content">
+                            <h6 class="custom_client">Brennette Abrenica</h6>
+                            <p style="color: black">Vice Chair</p>
+                        </div>
                     </div>
-                </div> -->
+                </div>
+                <div class="col-4">
+                    <div class="custom_testimonial-item">
+                        <div class="custom_testimonial-thumb">
+                            <img src="../assets/img/bod/abarquez.jpg" alt="client thumb">
+                        </div>
+                        <div class="testimonial-content">
+                            <h6 class="custom_client">Milagros Abarquez</h6>
+                            <p style="color: black">Member</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4">
+                    <div class="custom_testimonial-item">
+                        <div class="custom_testimonial-thumb">
+                            <img src="../assets/img/bod/mayormita.jpg" alt="client thumb">
+                        </div>
+                        <div class="testimonial-content">
+                            <h6 class="custom_client">Ricardo Mayormita</h6>
+                            <p style="color: black">Member</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="custom_testimonial-item">
+                        <div class="custom_testimonial-thumb">
+                            <img src="../assets/img/bod/yu.jpg" alt="client thumb">
+                        </div>
+                        <div class="testimonial-content">
+                            <h6 class="custom_client">Jesus Yu Jr.</h6>
+                            <p style="color: black">Member</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="custom_testimonial-item">
+                        <div class="custom_testimonial-thumb">
+                            <img src="../assets/img/bod/fornolles.jpg" alt="client thumb">
+                        </div>
+                        <div class="testimonial-content">
+                            <h6 class="custom_client">Marichu Fornolles</h6>
+                            <p style="color: black">Treasurer</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="custom_testimonial-item">
+                        <div class="custom_testimonial-thumb">
+                            <img src="../assets/img/bod/villaluz.jpg" alt="client thumb">
+                        </div>
+                        <div class="testimonial-content">
+                            <h6 class="custom_client">Celeste Villaluz</h6>
+                            <p style="color: black">Secretary</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
 </template>
 
 <script>
-    export default {
-        name: 'Video',
-        data () {
-            return {
-                video: "https://www.youtube.com/embed/iaj8ktgL3BY",
-                bgImg: {
-                    backgroundImage: `url(${require('../assets/img/bg/video-bg.jpg')})`
-                }
+export default {
+    name: 'Testimonial',
+    data (){
+        return {
+            bgImg: {
+                backgroundImage: `url(${require('../assets/img/bg/video-bg.jpg')})`
             }
         }
     }
+}
 </script>
 
 <style lang='scss'>
     @import '../assets/scss/variables.scss';
-    @import '../assets/scss/components/video.scss';
+    @import '../assets/scss/components/testimonial.scss';
+
+    .custom_client {
+        font-weight: 700;
+        color: black;
+        padding: 27px 0 10px;
+    }
+
+    .custom_testimonial-item {
+        text-align: center;
+        padding: 5%;
+    }
+
+    .custom_testimonial-thumb {
+        width: 200px;
+        height: 200px;
+        margin: 0 auto;
+        border-radius: 50%;
+        overflow: hidden;
+    }
 </style>
