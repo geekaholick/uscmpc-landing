40f9958d8097f2bd17d467d55f8ca1f7d4a8fbad
geekaholick <33748869+geekaholick@users.noreply.github.com>
2021-10-23 13:34:33 +0800

    T-13 Fix footer and nav.

--- src/uscmpc/Footer.vue
+++ src/uscmpc/Footer.vue
@@ -8,12 +8,35 @@
                             <h5 class="widget-title">ABOUT US</h5>
                             <div class="widget-body">
                                 <ul class="widget-info">
-                                    <li><strong>Adddress :</strong><span>Joseph Watzlawik Bldg., Pelaez St., Cebu City</span></li>
-                                    <li><strong>Phone 1 :</strong>(+63) 032 416 8032</li>
-                                    <li><strong>Phone 2:</strong>(+63) 032 384 0024</li>
-                                    <li><strong>Email :</strong>official.uscmpc@gmail.com</li>
-                                    <li><strong>Office Hours:</strong>Mon-Fri 8:00 AM - 5:00 PM</li>
-                                    <li><strong>Office Hours:</strong>    Sat 8:00 AM - 5:00 PM</li>
+                                    <li>
+                                        <strong
+                                            ><i class="fa fa-home"></i></strong
+                                        >:
+                                        <span
+                                            >Joseph Watzlawik Bldg., Pelaez St.,
+                                            Cebu City</span
+                                        >
+                                    </li>
+                                    <li>
+                                        <strong
+                                            ><i class="fa fa-phone"></i></strong
+                                        >: (+63) 032 416 8032 / (+63) 032 384
+                                        0024
+                                    </li>
+                                    <li>
+                                        <strong
+                                            ><i
+                                                class="fa fa-envelope-open"
+                                            ></i></strong
+                                        >: official.uscmpc@gmail.com
+                                    </li>
+                                    <li>
+                                        <strong
+                                            ><i
+                                                class="fa fa-briefcase"
+                                            ></i></strong
+                                        >: Mon-Sat 8:00 AM - 5:00 PM
+                                    </li>
                                 </ul>
                             </div>
                         </div>
@@ -21,14 +44,16 @@
 
                     <div class="col-lg-4">
                         <div class="widget-item">
-                            <h5 class="widget-title">OUR SERVICE</h5>
+                            <h5 class="widget-title">USEFUL LINKS</h5>
                             <div class="widget-body">
                                 <ul class="widget-info useful-link">
-                                    <li><a href="#">Lending</a></li>
+                                    <li><a href="/lending">Lending</a></li>
                                     <!-- <li><a href="#">Coop Education and Training</a></li> -->
-                                    <li><a href="#">Investment</a></li>
+                                    <li>
+                                        <a href="/coop-mart">Consumer Store</a>
+                                    </li>
                                     <!-- <li><a href="#">Transport Services</a></li> -->
-                                    <li><a href="#">Consumer Store</a></li>
+                                    <li><a href="#">Be a Member</a></li>
                                     <!-- <li><a href="#">Community Development</a></li> -->
                                 </ul>
                             </div>
@@ -37,17 +62,23 @@
 
                     <div class="col-lg-4">
                         <div class="widget-item">
-                            <h5 class="widget-title">OUR NEWSLETTER</h5>
+                            <h5 class="widget-title">OUR CHANNEL</h5>
                             <div class="widget-body newsletter-widget">
-                                <p>Please subscribe below if you would like to keep updated.</p>
-                                <form class="newsletter-form">
+                                <p>
+                                    Please subscribe below if you would like to
+                                    keep updated.
+                                </p>
+                                <!-- <form class="newsletter-form">
                                     <input type="text" class="newsletter-field" placeholder="Email Here">
                                     <button type="submit" class="subscribe-btn">SUBSCRIBE</button>
-                                </form>
+                                </form> -->
                                 <ul class="footer-social-link">
-                                    <li v-for="(social, socials) in socials" :key="socials">
+                                    <li
+                                        v-for="(social, socials) in socials"
+                                        :key="socials"
+                                    >
                                         <a :href="social.url" target="_blank">
-                                            <i :class=social.icon></i>
+                                            <i :class="social.icon"></i>
                                         </a>
                                     </li>
                                 </ul>
@@ -62,13 +93,22 @@
                 <div class="row">
                     <div class="col-md-6 order-2 order-md-1">
                         <div class="copyright">
-                            <p>&copy; 2021 <b class="text-white">USC&amp;C MPC</b> Made with <i class="fa fa-heart text-danger"></i> by <a href="#"><b>Gran, Gorii, Rex, and NJ</b></a></p>
+                            <p>
+                                &copy; 2021
+                                <b class="text-white"
+                                    >USC &amp; Community Multi-Purpose
+                                    Cooperative. All rights reserve.</b
+                                >
+                                <br />Made with
+                                <i class="fa fa-heart text-danger"></i> by
+                                <b>Technical Team</b>
+                            </p>
                         </div>
                     </div>
                     <div class="col-md-6 order-1 order-md-2">
                         <div class="useful-link">
-                            <a href="#">Our Team</a>
-                            <a href="#">Sight Map</a>
+                            <!-- <a href="#">Our Team</a>
+                            <a href="#">Sight Map</a> -->
                             <a href="http://event.uscmpc.com/login">Sign In</a>
                         </div>
                     </div>
@@ -80,14 +120,14 @@
 
 <script>
 export default {
-    name: 'Footer',
+    name: "Footer",
     props: ["FooterStyle"],
-    data () {
+    data() {
         return {
             socials: [
                 {
                     icon: "fa fa-facebook",
-                    url: "https://www.facebook.com/groups/347906875621343"
+                    url: "https://www.facebook.com/groups/347906875621343",
                 },
                 // {
                 //     icon: "fa fa-twitter",
@@ -101,13 +141,13 @@ export default {
                 //     icon: "fa fa-dribbble",
                 //     url: "https://dribbble.com/"
                 // }
-            ]
-        }
-    }
-}
+            ],
+        };
+    },
+};
 </script>
 
 <style lang='scss'>
-    @import '../assets/scss/variables.scss';
-    @import '../assets/scss/components/footer.scss';
+@import "../assets/scss/variables.scss";
+@import "../assets/scss/components/footer.scss";
 </style>
