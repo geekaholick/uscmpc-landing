57d2543a7a18c4d3040610557891cba9b98200af
Edrian Guanzon <guanzonedrian@gmail.com>
2022-03-01 20:45:16 +0800

    remove unecessary

--- src/uscmpc/Savings.vue
+++ src/uscmpc/Savings.vue
@@ -69,55 +69,7 @@ export default {
     return {
       bgImg: {
         backgroundImage: `url(${require("../assets/img/bg/screenshot-bg.jpg")})`,
-      },
-      swiperOption: {
-        speed: 600,
-        loop: true,
-        slidesPerView: 3,
-        effect: "coverflow",
-        // grabCursor: true,
-        centeredSlides: true,
-        coverflowEffect: {
-          rotate: -25,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: false,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-        breakpoints: {
-          768: {
-            spaceBetween: 60,
-          },
-          576: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-        },
-      },
-      // abouts: [
-      //   {
-      //     title: "FIXED DEPOSIT/SHARE CAPITAL",
-      //     desc: "As a member, he is allowed to make equity contributions to the capital fund of the COOP in the form of Share Capital. An applicant for membership shall subscribe at least Fifty (50) common shares at Php100.00 per share per year, in the amount of Five Thousand pesos (Php5,000.00) and pay the equivalent of at least Five (5) shares in the amount of Five hundred pesos (Php500.00) per month as his/her monthly contribution to the continuous capital buildup of the cooperative. However, no member shall own or hold more than ten percent (10%) of the total subscribed share capital of the Cooperative. CANNOT BE WITHDRAWN UNLESS THE MEMBER RETRACTS MEMBERSHIP",
-      //     test: "",
-      //     pic: require(`../assets/img/home-uscmpc/sharecap.png`),
-      //   },
-        // {
-        //     title: "DEPOSITS",
-        //     desc: "Money placements of members with the cooperative for a certain period of time. This can either be in the form of Regular Savings or Time Deposits.",
-        //     test: "you got that long hair sweat jeans thing amabob",
-        //     pic: require(`../assets/img/home-medical/app.png`),
-        // },
-        // {
-        //     title: "USC HIGH SCHOOL",
-        //     desc: "Nam liber tempor cum soluta nobis eleifend option cong nihil imperdiet.",
-        //     test: "you got that long hair sweat jeans thing amabob",
-        //     pic: require(`../assets/img/home-medical/app.png`),
-        // },
-      // ],
+      }
     };
   },
    computed: {
@@ -126,9 +78,7 @@ export default {
     })
   },
   created() {
-    this.getSavings().then(() => {
-      // this.sliders = this.savings;
-    });
+    this.getSavings();
   },
    methods: {
     ...mapActions({
